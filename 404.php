0c44b394e910eb60f591b0860121412b91b2e7bd
izumaru-sho <a.b.cdef1217is@gmail.com>
2022-07-07 11:33:52 +0900

    7/7修正

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
   <h2>404 Not Found（ページが見つかりませんでした）</h2>
   <p>指定された以下のページは存在しないか、または移動した可能性があります。</p>
   <p class="error_url">URL :<span><?php echo get_pagenum_link(); ?></span></p>
-  <p><a href="<?php echo home_url(); ?>">トップページへ</a></p>
+  <p><a href="<?php echo esc_url(home_url()); ?>">トップページへ</a></p>
 </div>
 <?php
 get_footer();
